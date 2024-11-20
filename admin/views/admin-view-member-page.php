a9a8626e8b0a9291678fd9005f138a1a6f7b82fa
4ndrisw <4ndr1sw@gmail.com>
2024-11-21 01:13:15 +0700

    Update admin-view-member-page.php

--- admin/views/admin-view-member-page.php
+++ admin/views/admin-view-member-page.php
@@ -7,7 +7,7 @@
  * Path: admin/views/admin-view-member-page.php
  * 
  * Changelog:
- * 2.2.0 - 2024-11-15
+ * 2.2.0 - 2024-11-15 11:20:13
  * - Removed image upload/delete forms
  * - Added Edit Photos button
  * - Enlarged image previews
